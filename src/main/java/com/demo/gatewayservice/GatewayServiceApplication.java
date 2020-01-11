b008f76d67fc237001372a311068c60cdd92a70a
sachchidatripathi <sntripathi01@gmail.com>
2020-01-11 08:31:24 -0700

    test commit for intelij

--- src/main/java/com/demo/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/demo/gatewayservice/GatewayServiceApplication.java
@@ -8,5 +8,6 @@ public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
+		System.out.println("hi");
 	}
 }
